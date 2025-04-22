86c5cf52cfdccdfa1325adf0004d39daf67ca98b
TiaWidi <tia.septianawidi@gmail.com>
2025-04-22 19:55:46 +0700

    Initial app.blade.php

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-SgOJa3DmI69IUzQ2PVdRZhwQ+dy64/BUtbMJw1MZ8t5HZApcHrRKUc4W0kG879m7" crossorigin="anonymous">
+    <title>@yield('title', 'Online Store')</title>
+</head>
+
+<body>
+    {{-- header --}}
+    <nav class="navbar navbar-expand-lg navbar-dark bg-secondary py-4">
+        <div class="container">
+            <a href="navbar-brand" href="#">Online Store</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav mas-auto">
+                    <a class="nav-link active" href="#">Home</a>
+                    <a class="nav-link active" href="#">About</a>
+                </div>
+            </div>
+        </div>
+    </nav>
+
+    <header class="masthead bg-primary text-white text-center py-4">
+        <div class="container d-flex align-items-center flex-coumn">
+            <h2>@yield('subtitle', 'A Laravel Online Store')</h2>
+        </div>
+    </header>
+    {{-- end-header --}}
+
+    <div class="container-my-4">
+        @yield('content')
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/js/bootstrap.bundle.min.js" integrity="sha384-k6d4wzSIapyDyv1kpU366/PK5hCdSbCRGRCMv+eplOQJWyd1fbcAu9OCUj5zNLiq" crossorigin="anonymous"></script>
+</body>
+
+</html>
