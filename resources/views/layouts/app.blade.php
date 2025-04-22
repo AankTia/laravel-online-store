146979a957467beb24f72dadf5a2c0af70e7967b
TiaWidi <tia.septianawidi@gmail.com>
2025-04-22 20:06:01 +0700

    Initial app.css

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-SgOJa3DmI69IUzQ2PVdRZhwQ+dy64/BUtbMJw1MZ8t5HZApcHrRKUc4W0kG879m7" crossorigin="anonymous">
+    <link rel="stylesheet" href="{{ asset('/css/app.css') }}">
     <title>@yield('title', 'Online Store')</title>
 </head>
 
@@ -35,6 +36,16 @@
     <div class="container-my-4">
         @yield('content')
     </div>
+
+    {{-- footer --}}
+    <div class="copyright py-4 text-center text-white">
+        <div class="container">
+            <small>
+                Copyright - <a class="text-reset fw-bold text-decoration-none" target="_blank" href="#">Tia Widi</a>
+            </small>
+        </div>
+    </div>
+    {{-- end-footer --}}
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/js/bootstrap.bundle.min.js" integrity="sha384-k6d4wzSIapyDyv1kpU366/PK5hCdSbCRGRCMv+eplOQJWyd1fbcAu9OCUj5zNLiq" crossorigin="anonymous"></script>
 </body>
