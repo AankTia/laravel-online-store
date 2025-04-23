afd38b0f76c6289472b6b82546950f7075bf5a09
TiaWidi <tia.septianawidi@gmail.com>
2025-04-23 08:46:41 +0700

    Initial Admin Layout

---
+++ resources/views/admin/home/index.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.admin')
+@section('title', $viewData["title"])
+@section('content')
+<div class="card-header">
+    Admin Panel - Home Page
+</div>
+<div class="card-body">
+    Welcome to the Admin Panel.
+</div>
+    
+@endsection
---
+++ resources/views/layouts/admin.blade.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-SgOJa3DmI69IUzQ2PVdRZhwQ+dy64/BUtbMJw1MZ8t5HZApcHrRKUc4W0kG879m7" crossorigin="anonymous">
+    <link rel="stylesheet" href="{{ asset('/css/admin.css') }}">
+
+    <title>@yield('title', 'Admin - Online Store')</title>
+</head>
+
+<body>
+    <div class="row g-0">
+        {{-- sidebar --}}
+        <div class="p-3 col fixed text-white bg-dark">
+            <a href="{{ route('admin.home.index') }}" class="text-white text-decoration-none">
+                <span class="fs-4">Admin Panel</span>
+            </a>
+            <hr />
+
+            <ul class="nav flex-column">
+                <li>
+                    <a href="{{ route('admin.home.index') }}" class="nav-link text-white">- Admin - Home</a>
+                </li>
+                <li>
+                    <a href="#" class="nav-link text-white">- Admin - Products</a>
+                </li>
+                <li>
+                    <a href="{{ route('home.index') }}" class="mt-2 btn bg-primary text-white">
+                        Go back to the home page
+                    </a>
+                </li>
+            </ul>
+        </div>
+        {{-- end sidebar --}}
+
+        <div class="col content-grey">
+            <nav class="p-3 shadow next-end">
+                <span class="profile-font">Admin</span>
+                <img class="img-profile rounded-circle" src="{{ asset('/img/undraw_profile.svg') }}" alt="">
+            </nav>
+
+            <div class="g-0 m-5">
+                @yield('content')
+            </div>
+        </div>
+    </div>
+
+    {{-- footer --}}
+    <div class="copyright py-4 text-center text-white">
+        <div class="container">
+            <small>
+                Copyright - <a class="text-reset fw-bold text-decoration-none" target="_blank" href="#">Tia Widi</a>
+            </small>
+        </div>
+    </div>
+    {{-- end-footer --}}
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/js/bootstrap.bundle.min.js" integrity="sha384-k6d4wzSIapyDyv1kpU366/PK5hCdSbCRGRCMv+eplOQJWyd1fbcAu9OCUj5zNLiq" crossorigin="anonymous"></script>
+</body>
+
+</html>
