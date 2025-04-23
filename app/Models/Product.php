88d04944d60a01287658c7ed45305142455cd747
TiaWidi <tia.septianawidi@gmail.com>
2025-04-23 09:21:08 +0700

    Refactor create product

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -6,16 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    /**
-     * PRODUCT ATTRIBUTE
-     * $this->attributes['id'] - int - contains the product primary key (id)
-     * $this->attributes['name'] - string - contains the product name
-     * $this->attributes['description'] - string - contains the product description
-     * $this->attributes['image'] - string - contains the product image
-     * $this->attributes['price'] - int - contains the product price
-     * $this->attributes['created_at'] - timestamp - contains the product creation date
-     * $this->attributes['updated_at'] - timestamp - contains the product update date
-     */
+    protected $fillable = [
+        'name',
+        'description',
+        'price',
+        'image'
+    ];
 
     public function getId()
     {
