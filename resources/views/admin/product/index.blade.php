d9e1259d9bd36ad24cd7d6ff3040923ef09c15ca
TiaWidi <tia.septianawidi@gmail.com>
2025-04-23 09:17:51 +0700

    Create Product

--- resources/views/admin/product/index.blade.php
+++ resources/views/admin/product/index.blade.php
@@ -1,9 +1,54 @@
 @extends('layouts.admin')
 @section('title', $viewData["title"])
 @section('content')
+<div class="card-mb-4">
+    <div class="card-header">
+        Create Products
+    </div>
+</div>
+
+<div class="card-body">
+    @if ($errors->any())
+    <ul class="alert alert-danger list-unstyled">
+        @foreach ($errors->all() as $error)
+        <li>- {{ $error }}</li>
+        @endforeach
+    </ul>
+    @endif
+</div>
+
+<form method="POST" action="{{ route('admin.product.store') }}">
+    @csrf
+    <div class="row">
+        <div class="col">
+            <div class="mb-3 row">
+                <label class="col-lg-2 col-md-6 com-sm-12 col-form-label">Name:</label>
+                <div class="col-lg-10 col-md-6 col-sm-12">
+                    <input type="text" name="name" value="{{ old('name')}}" class="form-control">
+                </div>
+            </div>
+        </div>
+        <div class="col">
+            <div class="mb-3 row">
+                <label class="col-lg-2 col-md-6 com-sm-12 col-form-label">Price:</label>
+                <div class="col-lg-10 col-md-6 col-sm-12">
+                    <input type="numer" name="price" value="{{ old('price')}}" class="form-control">
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="mb-3">
+        <label class="form-label">Description</label>
+        <textarea class="form-control" name="description" rows="3">{{ old('description') }}</textarea>
+    </div>
+
+    <button type="submit" class="btn btn-primary">Submit</button>
+</form>
+
 <div class="card-header">
     Manage Products
 </div>
+
 <div class="card-body">
     <table class="table table-bordered table-striped">
         <thead>
@@ -16,12 +61,12 @@
         </thead>
         <tbody>
             @foreach ($viewData["products"] as $product)
-                <tr>
-                    <td>{{ $product->getId() }}</td>
-                    <td>{{ $product->getName() }}</td>
-                    <td>Edit</td>
-                    <td>Delete</td>
-                </tr>
+            <tr>
+                <td>{{ $product->getId() }}</td>
+                <td>{{ $product->getName() }}</td>
+                <td>Edit</td>
+                <td>Delete</td>
+            </tr>
             @endforeach
         </tbody>
     </table>
